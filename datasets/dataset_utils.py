001dd61bb9501d0aa163fd92267cfb35f82ce966
Marius Kern <mariusskern@gmail.com>
2026-05-18 15:03:03 +0200

    Add iNaturalist to compare_vectors.ipynb

--- datasets/dataset_utils.py
+++ datasets/dataset_utils.py
@@ -15,6 +15,16 @@ class Transforms(Enum):
         torchvision.transforms.Normalize(mean=(0.48145466, 0.4578275, 0.40821073), std=(0.26862954, 0.26130258, 0.27577711))
     ])
 
+    CONVNEXT = torchvision.transforms.Compose([
+        torchvision.transforms.Resize(size=256, interpolation=torchvision.transforms.InterpolationMode.BICUBIC, max_size=None, antialias=True),
+        torchvision.transforms.CenterCrop(size=(224, 224)),
+        # torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize(
+            mean=(0.485, 0.456, 0.406),
+            std=(0.229, 0.224, 0.225),
+        ),
+    ])
+
     DINO_CLIP = torchvision.transforms.Compose([
         torchvision.transforms.Resize(224),
         torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
